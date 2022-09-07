98fe48f214adebbc8aae825da7a1413194082cfc
Prybrt06 <prybrt06@gmail.com>
2022-09-07 20:36:21 +0530

    Callout added

--- lib/widget/taskWidget.dart
+++ lib/widget/taskWidget.dart
@@ -1,32 +1,54 @@
 import 'package:flutter/material.dart';
 
-class task extends StatelessWidget {
+class task extends StatefulWidget {
   const task({Key? key}) : super(key: key);
+
+  @override
+  State<task> createState() => _taskState();
+}
+
+class _taskState extends State<task> {
+  bool isChecked = false;
+
+  void checkBoxCallBack(bool? checkBoxState) {
+    setState(() {
+      isChecked = checkBoxState ?? true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      // padding: EdgeInsets.only(
-      //   bottom: 20,
-      // ),
-      // child: Row(
-      //   children: [
-      //     Padding(
-      //       padding: EdgeInsets.only(
-      //         right: 10,
-      //       ),
-      //       child: Image(
-      //         image: AssetImage("assets/images/checkedLogo.png"),
-      //       ),
-      //     ),
-      //     Text("Beginning"),
-      //   ],
-      // ),
       child: ListTile(
-        leading: Image(
-          image: AssetImage("assets/images/checkedLogo.png"),
+        leading: Transform.scale(
+          scale: 1.3,
+          child: checkBox(isChecked, checkBoxCallBack),
         ),
-        title: Text("Beginning"),
+        title: Text(
+          "Beginning",
+          style: TextStyle(
+              fontSize: 18,
+              decoration: isChecked ? TextDecoration.lineThrough : null),
+        ),
+      ),
+    );
+  }
+}
+
+class checkBox extends StatelessWidget {
+  final bool isChecked;
+  final Function(bool?) toggleCheckBoxState;
+
+  checkBox(this.isChecked, this.toggleCheckBoxState);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Color(0xFFD89DCE),
+      value: isChecked,
+      onChanged: toggleCheckBoxState,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(6),
       ),
     );
   }
